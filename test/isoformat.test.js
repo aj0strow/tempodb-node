a6df2555c1f94dab8f77e29e63a31f871aa0ce87
AJ Ostrow <alexander.ostrow@gmail.com>
2014-05-01 01:47:11 -0400

    isoformat with moment

---
+++ test/isoformat.test.js
@@ -0,0 +1,19 @@
+var assert = require('assert')
+var moment = require('moment-timezone')
+var isoformat = require('../lib/isoformat')
+
+describe('lib/isoformat', function () {
+  function format (time) {
+    return isoformat(moment.tz(time, 'America/Montreal'))
+  }
+
+  it('should format date', function () {
+    var date = new Date(2014, 0, 3, 14, 23, 33, 0);
+    assert.equal('2014-01-03T14:23:33.000-0500', format(date))
+  })
+
+  it('should format string', function () {
+    var date = 'Jan 3, 2014 14:23:33'
+    assert.equal('2014-01-03T14:23:33.000-0500', format(date))
+  })
+})
